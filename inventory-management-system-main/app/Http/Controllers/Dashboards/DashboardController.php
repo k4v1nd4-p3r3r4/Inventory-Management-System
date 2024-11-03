409ce98f8c825f73d6f4afdc7f7219ff776e65f4
karunaamman <karunaamman2@gmail.com>
2024-11-04 01:44:12 +0530

    test

--- inventory-management-system-main/app/Http/Controllers/Dashboards/DashboardController.php
+++ inventory-management-system-main/app/Http/Controllers/Dashboards/DashboardController.php
@@ -16,7 +16,7 @@ class DashboardController extends Controller
     {
         $orders = DB::select("SELECT COUNT(*) as count FROM orders")[0]->count;
         $completedOrders = DB::select("SELECT COUNT(*) as count FROM orders WHERE order_status = 'COMPLETE'")[0]->count;
-
+//
         $products = DB::select("SELECT COUNT(*) as count FROM products")[0]->count;
 
         $purchases = DB::select("SELECT COUNT(*) as count FROM purchases")[0]->count;
