b966e3cb0afcf6379c3d30ec7b4cfbc9ddf5b680
Madhushan S Liyanage <127401521+madhushansliyanage@users.noreply.github.com>
2024-11-05 14:20:55 +0530

    Update DashboardController.php

--- inventory-management-system-main/app/Http/Controllers/Dashboards/DashboardController.php
+++ inventory-management-system-main/app/Http/Controllers/Dashboards/DashboardController.php
@@ -15,14 +15,23 @@ class DashboardController extends Controller
     public function index()
     {
 
-        $orders = DB::select("SELECT * FROM order_count_view")[0]->count;
-        $completedOrders = DB::select("SELECT * FROM completed_orders_count")[0]->count;
-        $products = DB::select("SELECT * FROM products_count")[0]->count;
-        $purchases = DB::select("SELECT * FROM purchases_count")[0]->count;
-        $todayPurchases = DB::select("SELECT * FROM today_purchases_count")[0]->count;
-        $categories = DB::select("SELECT * FROM categories_count")[0]->count;
-        $quotations = DB::select("SELECT * FROM quotations_count")[0]->count;
-        $todayQuotations = DB::select("SELECT * FROM today_quotations_count")[0]->count;
+        // $orders = DB::select("SELECT * FROM order_count_view")[0]->count;
+        // $completedOrders = DB::select("SELECT * FROM completed_orders_count")[0]->count;
+        // $products = DB::select("SELECT * FROM products_count")[0]->count;
+        // $purchases = DB::select("SELECT * FROM purchases_count")[0]->count;
+        // $todayPurchases = DB::select("SELECT * FROM today_purchases_count")[0]->count;
+        // $categories = DB::select("SELECT * FROM categories_count")[0]->count;
+        // $quotations = DB::select("SELECT * FROM quotations_count")[0]->count;
+        // $todayQuotations = DB::select("SELECT * FROM today_quotations_count")[0]->count;
+
+        $orders = DB::select("SELECT fn_OrdersCount()");
+        $completedOrders = DB::select("SELECT fn_OrdersCount()");
+        $products = DB::select("SELECT fn_ProductCount()");
+        $purchases = DB::select("SELECT fn_PurchsesCount()");
+        $todayPurchases = DB::select("SELECT fn_TodayPurchsesCount()");
+        $categories = DB::select("SELECT fn_CategoriesCount()");
+        $quotations = DB::select("SELECT fn_QuotationsCount()");
+        $todayQuotations = DB::select("SELECT fn_TodayQuotationsCount()");
 
         $monthlyOrders = collect(DB::select("
        SELECT
@@ -32,9 +41,7 @@ class DashboardController extends Controller
 FROM orders
 GROUP BY month
 ORDER BY MIN(order_date)
-LIMIT 12;
-
-    "));
+LIMIT 12;"));
 
     $monthlyPurchases = collect(DB::select("
        SELECT
